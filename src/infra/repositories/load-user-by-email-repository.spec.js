009005f33cf41a4a60348d275ef584908b6f6a6d
Cristiano <cristianovelkan@gmail.com>
2022-07-21 08:29:04 -0300

    move LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -41,4 +41,10 @@ describe('LoadUserByEmail Repository', () => {
       password: 'any_password'
     })
   })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
