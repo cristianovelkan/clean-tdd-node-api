b67705754b1623a71d381a78b0f4226c53b80d83
Cristiano <cristianovelkan@gmail.com>
2022-07-27 04:44:46 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,12 @@
 module.exports = (app) => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Headers', '*')
+    res.set(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    )
+    next()
+  })
 }
