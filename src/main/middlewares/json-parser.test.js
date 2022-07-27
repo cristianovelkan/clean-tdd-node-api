321a0fee653ea108975ca0d2016245ab8af2bb6f
Cristiano <cristianovelkan@gmail.com>
2022-07-27 04:54:47 -0300

    ensure requests parse body as JSON

---
+++ src/main/middlewares/json-parser.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('JSON Parser Middleware', () => {
+  test('Should parse body as JSON', async () => {
+    app.post('/test_json_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_json_parser')
+      .send({
+        name: 'John Doe'
+      })
+      .expect({ name: 'John Doe' })
+  })
+})
