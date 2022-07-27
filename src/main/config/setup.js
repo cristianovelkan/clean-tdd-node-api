8d305c2746a832e69830cdff455a0c72ced7615d
Cristiano <cristianovelkan@gmail.com>
2022-07-27 04:46:34 -0300

    move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,12 +1,5 @@
+const cors = require('../middlewares/cors')
 module.exports = (app) => {
   app.disable('x-powered-by')
-  app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.set('Access-Control-Allow-Headers', '*')
-    res.set(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-    )
-    next()
-  })
+  app.use(cors)
 }
